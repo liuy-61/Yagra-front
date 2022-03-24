fe5058a403a3cb9852dcbc5128045c9c3259f7b7
liuy-61 <1715052124@qq.com>
2022-03-24 17:02:37 +0800

    add methods in login.vue to access cookie.

--- src/views/login.vue
+++ src/views/login.vue
@@ -26,9 +26,35 @@ export default {
       (function(response){
           response.data
           console.log(response.data);
+          if (response.data.status_code == 0) {
+              alert("该用户未注册，请先注册");
+          }else if (response.data.status_code == 1){
+              alert("密码错误，请重新登录");
+          }else{
+              alert("登录成功");
+          }
       },function (err){
           console.log(err)
       })
+    },
+    setCookie(name, value, expireDays=1, ptah='/') {
+       var exp = new Date();
+       exp.setTime(exp.getTime() + expireDays*24*60*60*1000);
+       document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString() + ";path=" + ptah;
+    },
+    getCookie(name){
+        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+        if(arr=document.cookie.match(reg))
+        return unescape(arr[2]);
+        else
+        return null;
+    },
+     delCookie(name) {
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval=this.getCookie(name);
+        if(cval!=null)
+        document.cookie= name + "="+cval+";expires="+exp.toGMTString();
     }
   }
 }
